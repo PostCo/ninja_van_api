783fe2120ae7de29214f0a4a6044752b980ee1d2
Andy Chong <andygg1996personal@gmail.com>
2025-02-27 17:26:26 +0800

    Fix order resource by remove / in the beginning of path

--- lib/ninja_van_api/resources/order_resource.rb
+++ lib/ninja_van_api/resources/order_resource.rb
@@ -3,12 +3,12 @@
 module NinjaVanAPI
   class OrderResource < BaseResource
     def create(params = {})
-      response = post_request("/4.2/orders", body: params)
+      response = post_request("4.2/orders", body: params)
       Order.new(response.body)
     end
 
     def cancel(tracking_number)
-      response = delete_request("/2.2/orders/#{tracking_number}")
+      response = delete_request("2.2/orders/#{tracking_number}")
       Order.new(response.body)
     end
   end
